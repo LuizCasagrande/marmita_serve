a3d97e82954b404817be44f4fc7e2511974d2c13
usuario <juninhozatta221@gmail.com>
2019-12-11 13:52:05 -0300

    Revert "feito"
    This reverts commit ef7ed06b

--- src/main/java/com/marmitex/service/serviceImpl/PedidoServiceImpl.java
+++ src/main/java/com/marmitex/service/serviceImpl/PedidoServiceImpl.java
@@ -3,11 +3,14 @@ package com.marmitex.service.serviceImpl;
 import com.marmitex.data.PedidoData;
 import com.marmitex.event.PedidoPreSave;
 import com.marmitex.framework.CrudServiceImpl;
+import com.marmitex.model.Cliente;
 import com.marmitex.model.Pedido;
 import com.marmitex.service.PedidoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,6 +21,9 @@ public class PedidoServiceImpl extends CrudServiceImpl<Pedido, Long> implements 
 
     @Override
     public void preSave(Pedido entity) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Cliente cliente = (Cliente) authentication.getPrincipal();
+        entity.setCliente(cliente);
         applicationEventPublisher.publishEvent(new PedidoPreSave(this, entity));
     }
 
